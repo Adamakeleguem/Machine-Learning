4a6753c50c20cf698eaaf09666201b014f3b3a51
AdamaKeleguem <adamsouedsr@gmail.com>
2023-01-27 22:22:22 +0000

    item slider

--- Electro Shop/electro_shop/lib/screens/item_screen.dart
+++ Electro Shop/electro_shop/lib/screens/item_screen.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import '../widgets/product_images_slider.dart';
-
 class ItemScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,10 +22,36 @@ class ItemScreen extends StatelessWidget {
             ),
             child: Stack(
               children: [
-                Center(child: ProductImagesSlider(),)
+                Center(child: ProductImagesSlider(),),
+                Padding(padding: EdgeInsets.only(left: 15,top: 20),
+                child: InkWell(onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Icon(Icons.arrow_back_ios_new, size: 25,),
+                ),)
               ],
             ),
           ),
+          SizedBox(height: 10),
+          Padding(padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+            Text(
+              "Apple watch Series 6",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 22,
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(children: [
+              Container(
+                child: Row(children: [
+                  RatingBar.builder(itemBuilder: ((context, _) {},onRatingUpdate:(rating){}),)
+                ],),
+              )
+            ],)
+          ]),),
         ],
       ),
       ) ,
--- Electro Shop/electro_shop/lib/widgets/product_images_slider.dart
+++ Electro Shop/electro_shop/lib/widgets/product_images_slider.dart
@@ -4,10 +4,29 @@ class ProductImagesSlider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ImageSlideshow(
+      indicatorColor: Colors.redAccent,
+      indicatorBackgroundColor: Colors.white,
+      height: 300,
+      autoPlayInterval: 3000,
+      indicatorRadius: 4,
+      isLoop: true,
       children: [
         Padding(
           padding:EdgeInsets.all(8),
-          child: , )
+          child: Image.asset("images/p1.png"), 
+        ),
+        Padding(
+          padding:EdgeInsets.all(8),
+          child: Image.asset("images/p2.png"), 
+        ),
+        Padding(
+          padding:EdgeInsets.all(8),
+          child: Image.asset("images/p3.png"), 
+        ),
+        Padding(
+          padding:EdgeInsets.all(8),
+          child: Image.asset("images/p4.png"), 
+        )
       ],
     );
   }
